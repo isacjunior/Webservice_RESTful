6a50077a933fc727f6aa77a7859acdd2cf0f79d3
Isac <isac_asj91@hotmail.com>
2017-08-21 16:28:40 -0300

    create http methods

--- src/http/routes.js
+++ src/http/routes.js
@@ -3,6 +3,23 @@ const routes = (server) => {
     res.send('Hello World')
     next()
   })
+
+  server.get('categoria', (req, res, next) => {
+    res.send()
+    next()
+  })
+  server.post('categoria', (req, res, next) => {
+    res.send()
+    next()
+  })
+  // server.put('categoria', (req, res, next) => {
+  //   res.send()
+  //   next()
+  // })
+  // server.delete('categoria', (req, res, next) => {
+  //   res.send()
+  //   next()
+  // })
 }
 
 module.exports = routes
