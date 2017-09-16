48510fc328e77180834b3f7cb41ab621f2f6ba4d
Isac <isac_asj91@hotmail.com>
2017-09-16 13:21:45 -0300

    Create method post and fix next method in routes

--- src/http/routes.js
+++ src/http/routes.js
@@ -5,37 +5,36 @@ const routes = (server) => {
     db.categories().all()
       .then(categories => {
         res.send(categories)
-        next()
       })
       .catch(error => {
-        console.error(error)
-        next()
+        res.send(error)
       })
+    next()
   })
 
   server.post('categoria', (req, res, next) => {
     const { name } = req.params
-    server.get('categoria', (req, res, next) => {
-      db.categories().save(name)
-        .then(categories => {
-          res.send(categories)
-          next()
-        })
-        .catch(error => {
-          console.error(error)
-          next()
-        })
-    })
+    db.categories().save(name)
+      .then(categories => {
+        res.send(categories)
+      })
+      .catch(error => {
+        res.send(error)
+      })
+    next()
   })
 
-  // server.put('categoria', (req, res, next) => {
-  //   res.send()
-  //   next()
-  // })
-  // server.delete('categoria', (req, res, next) => {
-  //   res.send()
-  //   next()
-  // })
+  server.put('categoria', (req, res, next) => {
+    const { name, id } = req.params
+    db.categories().update(name, id)
+      .then(categories => {
+        res.send(categories)
+      })
+      .catch(error => {
+        res.send(error)
+      })
+    next()
+  })
 
   server.get('/', (req, res, next) => {
     res.send('Webservice com Node, Restify e MySql')
--- src/services/mysql/categories.js
+++ src/services/mysql/categories.js
@@ -1,5 +1,6 @@
 const categories = (deps) => {
   return {
+
     all: () => {
       return new Promise((resolve, reject) => {
         const { connection, errorHandler } = deps
@@ -8,13 +9,37 @@ const categories = (deps) => {
             errorHandler(error, 'Falha ao listar as categorias', reject)
             return false
           }
+          console.log(results)
           resolve({ categories: results })
         })
       })
     },
-    save: () => {},
-    update: () => {},
+    save: (name) => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps
+        connection.query('INSERT INTO categories (name) VALUES (?)', [name], (error, results) => {
+          if (error) {
+            errorHandler(error, `Falha ao inserir ${name} nas categorias`, reject)
+            return false
+          }
+          resolve({ category: { name, id: results.insertId } })
+        })
+      })
+    },
+    update: (name, id) => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps
+        connection.query('UPDATE categories SET name = ? WHERE id = ?', [name, id], (error, results) => {
+          if (error) {
+            errorHandler(error, `Falha ao atualizar a categoria ${name}`, reject)
+            return false
+          }
+          resolve({ category: { name } })
+        })
+      })
+    },
     del: () => {}
   }
 }
+
 module.exports = categories
